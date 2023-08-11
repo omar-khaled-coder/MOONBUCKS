da97e9bdb40ab17a1e4b104b725beb0e11e8f724
omar abdrbuo <omarkh3010@gmail.com>
2023-08-11 18:11:20 +0300

    done

---
+++ db/migrate/20230810145155_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.float :rating
+      t.text :description
+      t.string :short_description
+      t.string :price
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
